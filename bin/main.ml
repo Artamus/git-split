31a68ce8de3f882b767fea5cfd642f498ea7d98e
Markus Loide <markusloide@gmail.com>
2024-06-16 11:44:09 +0300

    Testing out autoformat on the main file.

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,10 @@
-let () =
-  print_endline "Hello, World!" in
-  let git_diff_cmd = Feather.process "git" [ "diff"; "HEAD~1"; "HEAD"] in
-  let stdout = git_diff_cmd |> Feather.collect Feather.stdout in
-  (* Now parse the diff. *)
-  let regex = Re.str "diff --git " |> Re.compile in
-  let splitted = Re.split regex stdout in
-  let first = List.hd splitted in
-  let second = List.hd (List.tl splitted) in
-  let () = print_endline first in
-  print_endline second
+let () = print_endline "Hello, World!" in
+let git_diff_cmd = Feather.process "git" [ "diff"; "HEAD~1"; "HEAD" ] in
+let stdout = git_diff_cmd |> Feather.collect Feather.stdout in
+(* Now parse the diff. *)
+let regex = Re.str "diff --git " |> Re.compile in
+let splitted = Re.split regex stdout in
+let first = List.hd splitted in
+let second = List.hd (List.tl splitted) in
+let () = print_endline first in
+print_endline second
