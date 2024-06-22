bd3f9d27f101a7cacbce64dde2d4ba931ce950cc
Markus Loide <markusloide@gmail.com>
2024-06-22 16:09:28 +0300

    Implement basic hunk selecting with the cursor.

--- lib/tui.ml
+++ lib/tui.ml
@@ -14,7 +14,7 @@ type line =
 
 type hunk = { lines : line list; included_in_diff : included_in_diff }
 type file = { path : string; hunks : hunk list; included_in_diff : included_in_diff }
-type cursor = FileCursor of int
+type cursor = FileCursor of int | HunkCursor of int * int
 type model = { files : file list; cursor : cursor }
 
 let initial_model =
@@ -115,20 +115,45 @@ let update event model =
       (model, Command.Seq [ Command.Exit_alt_screen; Command.Quit ])
   (* if we press up or `k`, we move up in the list. *)
   | Event.KeyDown ((Up | Key "k"), _modifier) ->
-      let cursor_pos = match model.cursor with FileCursor index -> index in
-      let new_cursor_pos = if cursor_pos = 0 then List.length model.files - 1 else cursor_pos - 1 in
-      let cursor = FileCursor new_cursor_pos in
+      let cursor =
+        match model.cursor with
+        | FileCursor 0 -> FileCursor (List.length model.files - 1)
+        | FileCursor file_idx -> FileCursor (file_idx - 1)
+        | HunkCursor (file_idx, 0) -> FileCursor file_idx
+        | HunkCursor (file_idx, hunk_idx) -> HunkCursor (file_idx, hunk_idx - 1)
+      in
+
       ({ model with cursor }, Command.Noop)
   (* if we press down or `j`, we move down in the list. *)
   | Event.KeyDown ((Down | Key "j"), _modifier) ->
-      let cursor_pos = match model.cursor with FileCursor index -> index in
-      let new_cursor_pos = if cursor_pos = List.length model.files - 1 then 0 else cursor_pos + 1 in
-      let cursor = FileCursor new_cursor_pos in
+      let last_file_idx = List.length model.files - 1 in
+      let cursor =
+        match model.cursor with
+        | FileCursor file_idx ->
+            if file_idx = last_file_idx then FileCursor 0 else FileCursor (file_idx + 1)
+        | HunkCursor (file_idx, hunk_idx) ->
+            let last_hunk_idx = List.length (List.nth model.files file_idx).hunks - 1 in
+            if hunk_idx = last_hunk_idx then
+              if file_idx = last_file_idx then FileCursor 0 else FileCursor (file_idx + 1)
+            else HunkCursor (file_idx, hunk_idx + 1)
+      in
       ({ model with cursor }, Command.Noop)
   (* if we press right or `l`, we expand the current item and move to the first subitem. *)
-  | Event.KeyDown ((Right | Key "l"), _modifier) -> (model, Command.Noop)
+  | Event.KeyDown ((Right | Key "l"), _modifier) ->
+      let cursor =
+        match model.cursor with
+        | FileCursor file_idx -> HunkCursor (file_idx, 0)
+        | HunkCursor (file_idx, hunk_idx) -> HunkCursor (file_idx, hunk_idx)
+      in
+      ({ model with cursor }, Command.Noop)
   (* if we press left or `h`, we move left in the list *)
-  | Event.KeyDown ((Left | Key "h"), _modifier) -> (model, Command.Noop)
+  | Event.KeyDown ((Left | Key "h"), _modifier) ->
+      let cursor =
+        match model.cursor with
+        | FileCursor file_idx -> FileCursor file_idx
+        | HunkCursor (file_idx, _) -> FileCursor file_idx
+      in
+      ({ model with cursor }, Command.Noop)
   (* when we press enter or space we toggle the item in the list
      that the cursor points to *)
   | Event.KeyDown ((Enter | Space), _modifier) ->
@@ -137,10 +162,30 @@ let update event model =
       in
       let files =
         List.mapi
-          (fun idx { path; hunks; included_in_diff } ->
-            let cursor_pos = match model.cursor with FileCursor index -> index in
-            let status = if idx = cursor_pos then toggle included_in_diff else included_in_diff in
-            { path; hunks; included_in_diff = status })
+          (fun file_idx { path; hunks; included_in_diff = file_included_in_diff } ->
+            let hunks =
+              List.mapi
+                (fun hunk_idx { lines; included_in_diff = hunk_included_in_diff } ->
+                  let included_in_diff =
+                    match model.cursor with
+                    | HunkCursor (file, hunk) ->
+                        if file = file_idx && hunk = hunk_idx then toggle hunk_included_in_diff
+                        else hunk_included_in_diff
+                    | FileCursor _file -> hunk_included_in_diff
+                  in
+
+                  { lines; included_in_diff })
+                hunks
+            in
+
+            let included_in_diff =
+              match model.cursor with
+              | FileCursor file ->
+                  if file = file_idx then toggle file_included_in_diff else file_included_in_diff
+              | HunkCursor (_, _) -> file_included_in_diff
+            in
+
+            { path; hunks; included_in_diff })
           model.files
       in
       ({ model with files }, Command.Noop)
@@ -148,14 +193,13 @@ let update event model =
   | _ -> (model, Command.Noop)
 
 let view model =
-  (* we create our options by mapping over them *)
   let lines =
     model.files
-    |> List.mapi (fun idx { path; hunks; included_in_diff } ->
+    |> List.mapi (fun file_idx { path; hunks; included_in_diff } ->
            (* Per file context. *)
-           let bla =
+           let hunks =
              List.mapi
-               (fun idx { lines; included_in_diff } ->
+               (fun hunk_idx { lines; included_in_diff } ->
                  (* Per hunk context. *)
                  let code_lines =
                    List.map
@@ -180,8 +224,13 @@ let view model =
                  in
 
                  let is_hunk_included_marker = if included_in_diff = Included then "x" else "" in
+                 let hunk_header_content =
+                   Format.sprintf "\t[%s] Hunk %d" is_hunk_included_marker (hunk_idx + 1)
+                 in
                  let hunk_header =
-                   Format.sprintf "\t[%s] Hunk %d" is_hunk_included_marker (idx + 1)
+                   if model.cursor = HunkCursor (file_idx, hunk_idx) then
+                     highlight "%s" hunk_header_content
+                   else hunk_header_content
                  in
                  hunk_header :: code_lines)
                hunks
@@ -189,12 +238,12 @@ let view model =
            in
 
            let is_file_included_marker = if included_in_diff = Included then "x" else "" in
-           let file_line = Format.sprintf "[%s] %s" is_file_included_marker path in
-           let cursor_pos = match model.cursor with FileCursor index -> index in
-           let maybe_highlighted_file_line =
-             if cursor_pos = idx then highlight "%s" file_line else file_line
+           let file_line_content = Format.sprintf "[%s] %s" is_file_included_marker path in
+           let file_line =
+             if model.cursor = FileCursor file_idx then highlight "%s" file_line_content
+             else file_line_content
            in
-           maybe_highlighted_file_line :: bla)
+           file_line :: hunks)
     |> List.flatten |> String.concat "\n"
   in
   let header =
