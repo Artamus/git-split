65258754c895aa371629379941800cf2e87dfee2
Markus Loide <markusloide@gmail.com>
2024-07-09 00:06:21 +0300

    Partially implement diff subtraction.
    This is a half baked implementation, because I will try and replace it instead if with just calling `git diff` over and over again!

--- lib/diff.ml
+++ lib/diff.ml
@@ -11,40 +11,69 @@ type renamed_file = { old_path : string; new_path : string; hunks : hunk list }
 type diff_file = RenamedFile of renamed_file | DiffFile of file [@@deriving show, eq]
 type diff = { files : diff_file list } [@@deriving show, eq]
 
-let subtract open_diff selected_diff =
+let contains_changes hunks =
+  hunks
+  |> List.exists (fun hunk ->
+         hunk.lines
+         |> List.exists (fun line ->
+                match line with RemovedLine _ | AddedLine _ -> true | _ -> false))
+
+let contains_changes diff =
+  diff.files
+  |> List.exists (fun file ->
+         match file with
+         | DiffFile diff_file -> contains_changes diff_file.hunks
+         | RenamedFile renamed_file -> contains_changes renamed_file.hunks)
+
+exception Internal_state_error of string
+
+let subtract_line ~open_line ~commit_line =
+  match (open_line, commit_line) with
+  | RemovedLine _, RemovedLine _ -> None
+  | AddedLine content, AddedLine _ -> Some (UnchangedLine content)
+  | _, _ -> raise (Internal_state_error "line filtering should have happened before")
+
+let subtract_hunk ~open_hunk ~commit_hunk =
+  open_hunk.lines
+  |> List.map (fun line ->
+         let maybe_commit_line =
+           commit_hunk.lines |> List.find_opt (fun commit_line -> commit_line = line)
+         in
+         match maybe_commit_line with None -> line | Some commit_line -> commit_line)
+
+let subtract_file ~open_file ~commit_file =
+  match (open_file, commit_file) with
+  | DiffFile open_diff_file, DiffFile commit_diff_file ->
+      let hunks =
+        open_diff_file.hunks
+        |> List.map (fun hunk ->
+               let maybe_commit_hunk =
+                 commit_diff_file.hunks
+                 |> List.find_opt (fun commit_hunk ->
+                        List.hd hunk.lines = List.hd commit_hunk.lines)
+               in
+               match maybe_commit_hunk with None -> hunk | Some commit_hunk -> commit_hunk)
+      in
+      DiffFile { path = open_diff_file.path; hunks }
+  | RenamedFile _, RenamedFile _ -> open_file
+  | _, _ -> raise (Internal_state_error "TODO")
+
+let subtract ~open_diff ~commit_diff =
   {
     files =
       open_diff.files
-      |> List.filter (fun file ->
+      |> List.map (fun file ->
              match file with
-             | DiffFile { path = open_diff_path; _ } -> (
-                 match
-                   List.find_opt
-                     (fun foo ->
-                       match foo with
-                       | DiffFile { path = selected_diff_path; _ } ->
-                           open_diff_path = selected_diff_path
-                       | _ -> false)
-                     selected_diff.files
-                 with
-                 | None -> true
-                 | Some _ -> false)
-             | RenamedFile { old_path = open_diff_old_path; new_path = open_diff_new_path; _ } -> (
-                 match
-                   List.find_opt
-                     (fun foo ->
-                       match foo with
-                       | RenamedFile
-                           {
-                             old_path = selected_diff_old_path;
-                             new_path = selected_diff_new_path;
-                             _;
-                           } ->
-                           open_diff_old_path = selected_diff_old_path
-                           && open_diff_new_path = selected_diff_new_path
-                       | _ -> false)
-                     selected_diff.files
-                 with
-                 | None -> true
-                 | Some _ -> false));
+             | DiffFile diff_file -> (
+                 let maybe_commit_file =
+                   commit_diff.files
+                   |> List.find_opt (fun commit_file ->
+                          match commit_file with
+                          | DiffFile commit_diff -> commit_diff.path = diff_file.path
+                          | RenamedFile _ -> false)
+                 in
+                 match maybe_commit_file with
+                 | None -> DiffFile diff_file
+                 | Some commit_file -> subtract_file ~open_file:file ~commit_file)
+             | RenamedFile renamed_file -> RenamedFile renamed_file);
   }
--- test/test_diff.ml
+++ test/test_diff.ml
@@ -3,7 +3,7 @@ open Git_split
 
 let diff_testable = testable Diff.pp_diff Diff.equal_diff
 
-let test_subtract_diff_from_itself () =
+let test_subtract_commit_is_same_as_open () =
   let open_diff : Diff.diff =
     {
       files =
@@ -27,7 +27,7 @@ let test_subtract_diff_from_itself () =
         ];
     }
   in
-  let selected_diff : Diff.diff =
+  let commit_diff : Diff.diff =
     {
       files =
         [
@@ -51,14 +51,168 @@ let test_subtract_diff_from_itself () =
     }
   in
 
-  let diff = Diff.subtract open_diff selected_diff in
+  let result_open_diff = Diff.subtract ~open_diff ~commit_diff in
 
   let expected : Diff.diff = { files = [] } in
-  check diff_testable "same diffs" diff expected
+  check diff_testable "same diffs" expected result_open_diff
+
+let test_subtract_commit_missing_removed () =
+  let open_diff : Diff.diff =
+    {
+      files =
+        [
+          DiffFile
+            {
+              path = "src/file";
+              hunks =
+                [
+                  {
+                    lines =
+                      [
+                        Diff.UnchangedLine "context";
+                        Diff.RemovedLine "removed-line";
+                        Diff.AddedLine "added-line";
+                        Diff.UnchangedLine "context";
+                      ];
+                  };
+                ];
+            };
+        ];
+    }
+  in
+  let commit_diff : Diff.diff =
+    {
+      files =
+        [
+          DiffFile
+            {
+              path = "src/file";
+              hunks =
+                [
+                  {
+                    lines =
+                      [
+                        Diff.UnchangedLine "context";
+                        Diff.UnchangedLine "removed-line";
+                        Diff.AddedLine "added-line";
+                        Diff.UnchangedLine "context";
+                      ];
+                  };
+                ];
+            };
+        ];
+    }
+  in
+
+  let result_open_diff = Diff.subtract ~open_diff ~commit_diff in
+
+  let expected : Diff.diff =
+    {
+      files =
+        [
+          DiffFile
+            {
+              path = "src/file";
+              hunks =
+                [
+                  {
+                    lines =
+                      [
+                        Diff.UnchangedLine "context";
+                        Diff.RemovedLine "removed-line";
+                        Diff.UnchangedLine "added-line";
+                        Diff.UnchangedLine "context";
+                      ];
+                  };
+                ];
+            };
+        ];
+    }
+  in
+  check diff_testable "same diffs" expected result_open_diff
+
+let test_subtract_commit_missing_added () =
+  let open_diff : Diff.diff =
+    {
+      files =
+        [
+          DiffFile
+            {
+              path = "src/file";
+              hunks =
+                [
+                  {
+                    lines =
+                      [
+                        Diff.UnchangedLine "context";
+                        Diff.RemovedLine "removed-line";
+                        Diff.AddedLine "added-line";
+                        Diff.UnchangedLine "context";
+                      ];
+                  };
+                ];
+            };
+        ];
+    }
+  in
+  let commit_diff : Diff.diff =
+    {
+      files =
+        [
+          DiffFile
+            {
+              path = "src/file";
+              hunks =
+                [
+                  {
+                    lines =
+                      [
+                        Diff.UnchangedLine "context";
+                        Diff.RemovedLine "removed-line";
+                        Diff.UnchangedLine "context";
+                      ];
+                  };
+                ];
+            };
+        ];
+    }
+  in
+
+  let result_open_diff = Diff.subtract ~open_diff ~commit_diff in
+
+  let expected : Diff.diff =
+    {
+      files =
+        [
+          DiffFile
+            {
+              path = "src/file";
+              hunks =
+                [
+                  {
+                    lines =
+                      [
+                        Diff.UnchangedLine "context";
+                        Diff.AddedLine "added-line";
+                        Diff.UnchangedLine "context";
+                      ];
+                  };
+                ];
+            };
+        ];
+    }
+  in
+  check diff_testable "same diffs" expected result_open_diff
 
 let diff_suite =
   [
     ( "when subtracting a diff from itself, the result is an empty diff",
       `Quick,
-      test_subtract_diff_from_itself );
+      test_subtract_commit_is_same_as_open );
+    ( "when the commit doesn't contain the removed line, it stays in the open diff",
+      `Quick,
+      test_subtract_commit_missing_removed );
+    ( "when the commit doesn't contain the added line, it stays in the open diff",
+      `Quick,
+      test_subtract_commit_missing_added );
   ]
