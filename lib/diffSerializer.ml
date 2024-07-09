f4c64c1add67f164ddff7e3b848164cb033043c6
Markus Loide <markusloide@gmail.com>
2024-07-09 21:20:04 +0300

    Implement hunk header lines calculation.

--- lib/diffSerializer.ml
+++ lib/diffSerializer.ml
@@ -6,7 +6,32 @@ let serialize_line = function
   | AddedLine content -> Printf.sprintf "+%s" content
 
 let serialize_hunk hunk =
-  let hunk_header = Printf.sprintf "@@ -0,4 +0,4 @@" in
+  let minus_startline = hunk.first_line_idx in
+  let minus_endline =
+    List.fold_left
+      (fun acc line ->
+        match line with UnchangedLine _ -> acc + 1 | RemovedLine _ -> acc + 1 | AddedLine _ -> acc)
+      (minus_startline - 1) hunk.lines
+  in
+
+  let plus_startline = minus_startline in
+  let plus_endline =
+    List.fold_left
+      (fun acc line ->
+        match line with UnchangedLine _ -> acc + 1 | RemovedLine _ -> acc | AddedLine _ -> acc + 1)
+      (plus_startline - 1) hunk.lines
+  in
+
+  let left_line_indices =
+    if minus_startline = minus_endline then Printf.sprintf "-%d" minus_startline
+    else Printf.sprintf "-%d,%d" minus_startline minus_endline
+  in
+  let right_line_indices =
+    if plus_startline = plus_endline then Printf.sprintf "+%d" plus_startline
+    else Printf.sprintf "+%d,%d" plus_startline plus_endline
+  in
+
+  let hunk_header = Printf.sprintf "@@ %s %s @@" left_line_indices right_line_indices in
   let lines = hunk.lines |> List.map serialize_line in
   hunk_header :: lines |> String.concat "\n"
 
--- test/test_diffSerializer.ml
+++ test/test_diffSerializer.ml
@@ -1,7 +1,7 @@
 open Alcotest
 open Git_split
 
-let test_serializes_single_hunk () =
+let test_serializes_single_hunk_with_context () =
   let diff : Diff.diff =
     {
       files =
@@ -12,7 +12,7 @@ let test_serializes_single_hunk () =
               hunks =
                 [
                   {
-                    first_line_idx = 0;
+                    first_line_idx = 1;
                     lines =
                       [
                         Diff.UnchangedLine "context";
@@ -33,7 +33,7 @@ let test_serializes_single_hunk () =
     "diff --git a/src/main b/src/main\n\
      --- a/src/main\n\
      +++ b/src/main\n\
-     @@ -0,4 +0,4 @@\n\
+     @@ -1,3 +1,3 @@\n\
     \ context\n\
      -removed-line\n\
      +added-line\n\
@@ -41,5 +41,91 @@ let test_serializes_single_hunk () =
   in
   check string "same git diffs" expected git_diff
 
+let test_serializes_single_hunk_starting_with_removed_line () =
+  let diff : Diff.diff =
+    {
+      files =
+        [
+          DiffFile
+            {
+              path = "src/main";
+              hunks =
+                [
+                  {
+                    first_line_idx = 1;
+                    lines =
+                      [
+                        Diff.RemovedLine "removed-line";
+                        Diff.RemovedLine "removed-line-2";
+                        Diff.UnchangedLine "context";
+                      ];
+                  };
+                ];
+            };
+        ];
+    }
+  in
+
+  let git_diff = DiffSerializer.serialize diff in
+
+  let expected =
+    "diff --git a/src/main b/src/main\n\
+     --- a/src/main\n\
+     +++ b/src/main\n\
+     @@ -1,3 +1 @@\n\
+     -removed-line\n\
+     -removed-line-2\n\
+    \ context"
+  in
+  check string "same git diffs" expected git_diff
+
+let test_serializes_single_hunk_starting_with_added_line () =
+  let diff : Diff.diff =
+    {
+      files =
+        [
+          DiffFile
+            {
+              path = "src/main";
+              hunks =
+                [
+                  {
+                    first_line_idx = 1;
+                    lines =
+                      [
+                        Diff.AddedLine "added-line";
+                        Diff.AddedLine "added-line-2";
+                        Diff.UnchangedLine "context";
+                      ];
+                  };
+                ];
+            };
+        ];
+    }
+  in
+
+  let git_diff = DiffSerializer.serialize diff in
+
+  let expected =
+    "diff --git a/src/main b/src/main\n\
+     --- a/src/main\n\
+     +++ b/src/main\n\
+     @@ -1 +1,3 @@\n\
+     +added-line\n\
+     +added-line-2\n\
+    \ context"
+  in
+  check string "same git diffs" expected git_diff
+
 let diff_serializer_suite =
-  [ ("serializes a diff with a single hunk", `Quick, test_serializes_single_hunk) ]
+  [
+    ( "serializes a hunk that starts with a context line",
+      `Quick,
+      test_serializes_single_hunk_with_context );
+    ( "serializes a hunk that starts with a removed line",
+      `Quick,
+      test_serializes_single_hunk_starting_with_removed_line );
+    ( "serializes a hunk that starts with an added line",
+      `Quick,
+      test_serializes_single_hunk_starting_with_added_line );
+  ]
