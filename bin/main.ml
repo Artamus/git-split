bb28840a896ae302ad4c1f29c274bc27a7a8a541
Markus Loide <markusloide@gmail.com>
2024-07-17 22:35:22 +0300

    Add initial support for splitting a commit that is not at the end of a chain.

--- bin/main.ml
+++ bin/main.ml
@@ -2,18 +2,64 @@ open Git_split
 open Feather
 open Feather.Infix
 
-let rec select_changes current_diff_commit target_commit =
-  let head_diff = process "git" [ "diff"; current_diff_commit; target_commit ] |> collect stdout in
-  if String.length head_diff = 0 then ()
+let usage_msg = "git-split [commit-id]"
+let commit_id = ref "HEAD"
+let anon_fun selected_commit = commit_id := selected_commit
+let speclist = [ ("[commit-id]", Arg.Set_string commit_id, "Commit to split. Defaults to HEAD.") ]
+
+type head_commit = { reference_commit : string; target_commit : string }
+
+type intermediary_commit = {
+  reference_commit : string;
+  target_commit : string;
+  original_head : string;
+  branch : string;
+}
+
+type split_commit = HeadCommit of head_commit | IntermediaryCommit of intermediary_commit
+
+let cleanup intermediary_commit =
+  process "git"
+    [
+      "rebase";
+      "--onto";
+      intermediary_commit.reference_commit;
+      intermediary_commit.target_commit ^ "~1";
+      intermediary_commit.original_head;
+    ]
+  |> run;
+  let head_commit_id = process "git" [ "rev-parse"; "--short"; "HEAD" ] |> collect stdout in
+  process "git" [ "branch"; "-f"; intermediary_commit.branch; head_commit_id ] |> run;
+  process "git" [ "switch"; intermediary_commit.branch ] |> run
+
+let rec select_changes (split_commit : split_commit) =
+  let reference_commit =
+    match split_commit with
+    | HeadCommit { reference_commit; _ } -> reference_commit
+    | IntermediaryCommit { reference_commit; _ } -> reference_commit
+  in
+  let target_commit =
+    match split_commit with
+    | HeadCommit { target_commit; _ } -> target_commit
+    | IntermediaryCommit { target_commit; _ } -> target_commit
+  in
+  let head_diff = process "git" [ "diff"; reference_commit; target_commit ] |> collect stdout in
+  if String.length head_diff = 0 then
+    let () =
+      match split_commit with
+      | HeadCommit _ -> ()
+      | IntermediaryCommit intermediary_commit -> cleanup intermediary_commit
+    in
+    print_endline "Job's done!"
   else
     let diff = DiffParser.parse_diff head_diff in
     let final_model = NottyTui.run @@ NottyTui.model_of_diff diff in
     match final_model with
-    | None -> print_endline "Job's done!"
+    | None -> print_endline "Aborted"
     | Some model ->
         let selected_diff = NottyTui.diff_of_model model in
         let serialized_diff = DiffSerializer.serialize selected_diff in
-        let tmp_filename = Printf.sprintf "/tmp/%s.diff" current_diff_commit in
+        let tmp_filename = Printf.sprintf "/tmp/%s.diff" reference_commit in
         echo serialized_diff > tmp_filename |> run;
         process "git" [ "apply"; tmp_filename ] |> run;
         process "git" [ "add"; "." ] |> run;
@@ -21,16 +67,43 @@ let rec select_changes current_diff_commit target_commit =
         let current_diff_commit =
           process "git" [ "rev-parse"; "--short"; "HEAD" ] |> collect stdout
         in
-        select_changes current_diff_commit target_commit
+        let foo =
+          match split_commit with
+          | HeadCommit head_commit ->
+              HeadCommit { head_commit with reference_commit = current_diff_commit }
+          | IntermediaryCommit intermediary_commit ->
+              IntermediaryCommit { intermediary_commit with reference_commit = current_diff_commit }
+        in
+        select_changes foo
 
-let main () =
-  (* Only HEAD for now. *)
-  let splittable_commit_id = process "git" [ "rev-parse"; "--short"; "HEAD" ] |> collect stdout in
-  let starting_diff_commit_id =
-    process "git" [ "rev-parse"; "--short"; splittable_commit_id ^ "~1" ] |> collect stdout
+let () =
+  Arg.parse speclist anon_fun usage_msg;
+
+  let starting_reference_commit_id =
+    process "git" [ "rev-parse"; "--short"; !commit_id ^ "~1" ] |> collect stdout
   in
-  print_endline @@ "Resetting commit " ^ splittable_commit_id;
-  process "git" [ "reset"; "--hard"; "HEAD~1" ] |> run;
-  select_changes starting_diff_commit_id splittable_commit_id
+  let head_commit_id = process "git" [ "rev-parse"; "--short"; "HEAD" ] |> collect stdout in
+  let split_commit =
+    if !commit_id = "HEAD" then
+      HeadCommit { reference_commit = starting_reference_commit_id; target_commit = head_commit_id }
+    else
+      let branch = process "git" [ "rev-parse"; "--abbrev-ref"; "HEAD" ] |> collect stdout in
+      IntermediaryCommit
+        {
+          reference_commit = starting_reference_commit_id;
+          target_commit = !commit_id;
+          original_head = head_commit_id;
+          branch;
+        }
+  in
 
-let () = main ()
+  let () =
+    match split_commit with
+    | HeadCommit { reference_commit; _ } ->
+        print_endline @@ "Resetting commit " ^ !commit_id;
+        process "git" [ "reset"; "--hard"; reference_commit ] |> run
+    | IntermediaryCommit { reference_commit; _ } ->
+        process "git" [ "-c"; "advice.detachedHead=false"; "checkout"; reference_commit ] |> run
+  in
+
+  select_changes split_commit
