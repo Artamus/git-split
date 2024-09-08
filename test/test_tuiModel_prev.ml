5692a6649b538914a9a55a532af6c0c1cde2b6fc
Markus Loide <markusloide@gmail.com>
2024-09-08 22:24:36 +0300

    Add some more tests for "prev" and reorganize the tests slightly.

--- test/test_tuiModel_prev.ml
+++ test/test_tuiModel_prev.ml
@@ -6,7 +6,7 @@ let tui_model_testable = testable TuiModel.pp_model TuiModel.equal_model
 (* These tests define the raw model types in variables so that they are easily reusable in the zippers.
    This is necessary because the way the zippers are used creates data duplication and this prevents mismatches between the raw data and the one in zippers. *)
 
-let test_to_last_file () =
+let test_file_to_prev_file () =
   let first_file : TuiTypes.file =
     {
       path = FilePath "src/main";
@@ -37,17 +37,23 @@ let test_to_last_file () =
         ];
     }
   in
-  let model : TuiModel.model = TuiModel.File (Zipper.Zip ([], first_file, [ second_file ])) in
+  let model : TuiModel.model = TuiModel.File (Zipper.Zip ([ first_file ], second_file, [])) in
 
   let prev_model = TuiModel.prev model in
 
-  let expected = TuiModel.File (Zipper.Zip ([ first_file ], second_file, [])) in
+  let expected = TuiModel.File (Zipper.Zip ([], first_file, [ second_file ])) in
   check tui_model_testable "same TUI models" expected prev_model
 
-let test_to_last_file_last_hunk () =
+let test_file_to_prev_hunk () =
+  let first_file_hunk : TuiTypes.hunk =
+    { starting_line = 1; context_snippet = None; visibility = Collapsed; lines = [] }
+  in
   let first_file : TuiTypes.file =
+    { path = FilePath "src/main"; visibility = Expanded; hunks = [ first_file_hunk ] }
+  in
+  let second_file : TuiTypes.file =
     {
-      path = FilePath "src/main";
+      path = FilePath "src/test";
       visibility = Collapsed;
       hunks =
         [
@@ -60,31 +66,32 @@ let test_to_last_file_last_hunk () =
         ];
     }
   in
-  let second_file_hunk : TuiTypes.hunk =
-    {
-      starting_line = 1;
-      context_snippet = None;
-      visibility = Collapsed;
-      lines = [ Diff ("code", `added, `included) ];
-    }
-  in
-  let second_file : TuiTypes.file =
-    { path = FilePath "src/test"; visibility = Expanded; hunks = [ second_file_hunk ] }
-  in
-  let model : TuiModel.model = TuiModel.File (Zipper.Zip ([], first_file, [ second_file ])) in
+  let model : TuiModel.model = TuiModel.File (Zipper.Zip ([ first_file ], second_file, [])) in
 
   let prev_model = TuiModel.prev model in
 
   let expected =
     TuiModel.Hunk
-      (Zipper.Zip ([ first_file ], second_file, []), Zipper.Zip ([], second_file_hunk, []))
+      (Zipper.Zip ([], first_file, [ second_file ]), Zipper.Zip ([], first_file_hunk, []))
   in
   check tui_model_testable "same TUI models" expected prev_model
 
-let test_to_last_file_last_hunk_last_line () =
-  let first_file : TuiTypes.file =
+let test_file_to_prev_line () =
+  let first_file_line : TuiTypes.line = Diff ("code", `added, `included) in
+  let first_file_hunk : TuiTypes.hunk =
     {
-      path = FilePath "src/main";
+      starting_line = 1;
+      context_snippet = None;
+      visibility = Expanded;
+      lines = [ first_file_line ];
+    }
+  in
+  let first_file : TuiTypes.file =
+    { path = FilePath "src/main"; visibility = Expanded; hunks = [ first_file_hunk ] }
+  in
+  let second_file : TuiTypes.file =
+    {
+      path = FilePath "src/test";
       visibility = Collapsed;
       hunks =
         [
@@ -97,27 +104,15 @@ let test_to_last_file_last_hunk_last_line () =
         ];
     }
   in
-  let second_file_line : TuiTypes.line = Diff ("code", `added, `included) in
-  let second_file_hunk : TuiTypes.hunk =
-    {
-      starting_line = 1;
-      context_snippet = None;
-      visibility = Expanded;
-      lines = [ second_file_line ];
-    }
-  in
-  let second_file : TuiTypes.file =
-    { path = FilePath "src/test"; visibility = Expanded; hunks = [ second_file_hunk ] }
-  in
-  let model : TuiModel.model = TuiModel.File (Zipper.Zip ([], first_file, [ second_file ])) in
+  let model : TuiModel.model = TuiModel.File (Zipper.Zip ([ first_file ], second_file, [])) in
 
   let prev_model = TuiModel.prev model in
 
   let expected =
     TuiModel.Line
-      ( Zipper.Zip ([ first_file ], second_file, []),
-        Zipper.Zip ([], second_file_hunk, []),
-        LineZipper.Zip ([], second_file_line, []) )
+      ( Zipper.Zip ([], first_file, [ second_file ]),
+        Zipper.Zip ([], first_file_hunk, []),
+        LineZipper.Zip ([], first_file_line, []) )
   in
   check tui_model_testable "same TUI models" expected prev_model
 
@@ -142,7 +137,7 @@ let test_hunk_to_file () =
   let expected = TuiModel.File (Zipper.Zip ([], file, [])) in
   check tui_model_testable "same TUI models" expected prev_model
 
-let test_hunk_to_hunk () =
+let test_hunk_to_prev_hunk () =
   let first_hunk : TuiTypes.hunk =
     {
       starting_line = 1;
@@ -173,7 +168,7 @@ let test_hunk_to_hunk () =
   in
   check tui_model_testable "same TUI models" expected prev_model
 
-let test_hunk_to_line () =
+let test_hunk_to_prev_line () =
   let first_hunk_line : TuiTypes.line = Diff ("code", `added, `included) in
   let first_hunk : TuiTypes.hunk =
     {
@@ -226,7 +221,7 @@ let test_line_to_hunk () =
   let expected = TuiModel.Hunk (Zipper.Zip ([], file, []), Zipper.Zip ([], hunk, [])) in
   check tui_model_testable "same TUI models" expected prev_model
 
-let test_line_to_line () =
+let test_line_to_prev_line () =
   let first_line : TuiTypes.line = Diff ("code", `added, `included) in
   let second_line : TuiTypes.line = Diff ("code2", `added, `included) in
   let hunk : TuiTypes.hunk =
@@ -257,18 +252,138 @@ let test_line_to_line () =
   in
   check tui_model_testable "same TUI models" expected prev_model
 
+let test_first_file_to_last_file () =
+  let first_file : TuiTypes.file =
+    {
+      path = FilePath "src/main";
+      visibility = Collapsed;
+      hunks =
+        [
+          {
+            starting_line = 1;
+            context_snippet = None;
+            visibility = Collapsed;
+            lines = [ Diff ("code", `added, `included) ];
+          };
+        ];
+    }
+  in
+  let second_file : TuiTypes.file =
+    {
+      path = FilePath "src/test";
+      visibility = Collapsed;
+      hunks =
+        [
+          {
+            starting_line = 1;
+            context_snippet = None;
+            visibility = Collapsed;
+            lines = [ Diff ("code", `added, `included) ];
+          };
+        ];
+    }
+  in
+  let model : TuiModel.model = TuiModel.File (Zipper.Zip ([], first_file, [ second_file ])) in
+
+  let prev_model = TuiModel.prev model in
+
+  let expected = TuiModel.File (Zipper.Zip ([ first_file ], second_file, [])) in
+  check tui_model_testable "same TUI models" expected prev_model
+
+let test_first_file_to_last_file_last_hunk () =
+  let first_file : TuiTypes.file =
+    {
+      path = FilePath "src/main";
+      visibility = Collapsed;
+      hunks =
+        [
+          {
+            starting_line = 1;
+            context_snippet = None;
+            visibility = Collapsed;
+            lines = [ Diff ("code", `added, `included) ];
+          };
+        ];
+    }
+  in
+  let second_file_hunk : TuiTypes.hunk =
+    {
+      starting_line = 1;
+      context_snippet = None;
+      visibility = Collapsed;
+      lines = [ Diff ("code", `added, `included) ];
+    }
+  in
+  let second_file : TuiTypes.file =
+    { path = FilePath "src/test"; visibility = Expanded; hunks = [ second_file_hunk ] }
+  in
+  let model : TuiModel.model = TuiModel.File (Zipper.Zip ([], first_file, [ second_file ])) in
+
+  let prev_model = TuiModel.prev model in
+
+  let expected =
+    TuiModel.Hunk
+      (Zipper.Zip ([ first_file ], second_file, []), Zipper.Zip ([], second_file_hunk, []))
+  in
+  check tui_model_testable "same TUI models" expected prev_model
+
+let test_first_file_to_last_file_last_hunk_last_line () =
+  let first_file : TuiTypes.file =
+    {
+      path = FilePath "src/main";
+      visibility = Collapsed;
+      hunks =
+        [
+          {
+            starting_line = 1;
+            context_snippet = None;
+            visibility = Collapsed;
+            lines = [ Diff ("code", `added, `included) ];
+          };
+        ];
+    }
+  in
+  let second_file_line : TuiTypes.line = Diff ("code", `added, `included) in
+  let second_file_hunk : TuiTypes.hunk =
+    {
+      starting_line = 1;
+      context_snippet = None;
+      visibility = Expanded;
+      lines = [ second_file_line ];
+    }
+  in
+  let second_file : TuiTypes.file =
+    { path = FilePath "src/test"; visibility = Expanded; hunks = [ second_file_hunk ] }
+  in
+  let model : TuiModel.model = TuiModel.File (Zipper.Zip ([], first_file, [ second_file ])) in
+
+  let prev_model = TuiModel.prev model in
+
+  let expected =
+    TuiModel.Line
+      ( Zipper.Zip ([ first_file ], second_file, []),
+        Zipper.Zip ([], second_file_hunk, []),
+        LineZipper.Zip ([], second_file_line, []) )
+  in
+  check tui_model_testable "same TUI models" expected prev_model
+
 let prev_suite =
   [
-    ("moves the cursor from the first file to the last file", `Quick, test_to_last_file);
+    ("moves the cursor from a file to the previous file", `Quick, test_file_to_prev_file);
+    ("moves the cursor from a file to the previous file's last hunk", `Quick, test_file_to_prev_hunk);
+    ( "moves the cursor from a file to the previous file's last hunk's last line",
+      `Quick,
+      test_file_to_prev_line );
+    ("moves the cursor from a hunk to its file", `Quick, test_hunk_to_file);
+    ("moves the cursor from a hunk to the previous hunk", `Quick, test_hunk_to_prev_hunk);
+    ("moves the cursor from a hunk to the previous hunk's last line", `Quick, test_hunk_to_prev_line);
+    ("moves the cursor from a line to its hunk", `Quick, test_line_to_hunk);
+    ("moves the cursor from a line to the previous line", `Quick, test_line_to_prev_line);
+    ("moves the cursor from the first file to the last file", `Quick, test_first_file_to_last_file);
     ( "moves the cursor from the first file to the last file's last hunk",
       `Quick,
-      test_to_last_file_last_hunk );
-    ( "moves the cursor from the first file to the last hunk's last line",
+      test_first_file_to_last_file_last_hunk );
+    ( "moves the cursor from the first file to the last file's last hunk's last line",
       `Quick,
-      test_to_last_file_last_hunk_last_line );
-    ("moves the cursor from a hunk to its file", `Quick, test_hunk_to_file);
-    ("moves the cursor from a hunk to the previous hunk", `Quick, test_hunk_to_hunk);
-    ("moves the cursor from a hunk to the previous hunk's last line", `Quick, test_hunk_to_line);
-    ("moves the cursor from a line to its hunk", `Quick, test_line_to_hunk);
-    ("moves the cursor from a line to the previous line", `Quick, test_line_to_line);
+      test_first_file_to_last_file_last_hunk_last_line );
   ]
